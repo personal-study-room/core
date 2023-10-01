610aeb4c60dc7cd9196de26c42dfe45b3b689f2a
hongsh429 <shh0429@kakao.com>
2023-10-01 23:22:01 +0900

    Spring은 Bean을 어떻게 관리할까?  - singleton Pattern 사용!  - singleton 아니라면, 요청만큼 인스턴스를 생성할 것이다. -> spring이 singleton pattern을 채택한 이유

---
+++ src/test/java/hello/core/singleton/SingletonService.java
@@ -0,0 +1,17 @@
+package hello.core.singleton;
+
+public class SingletonService {
+
+    private static final SingletonService instance = new SingletonService();
+
+    public static SingletonService getInstance() {
+        return instance;
+    }
+
+    // 생성자를 private으로 해서 더이상 인스턴스를 생성하지 못하도록 한다.
+    private SingletonService() {}
+
+    public void logic() {
+        System.out.println("싱글톤 객체 로직 호출");
+    }
+}
