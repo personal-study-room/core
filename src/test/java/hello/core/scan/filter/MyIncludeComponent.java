dc287d4f118ba9c24b140182f251bc7e70332722
hongsh429 <shh0429@kakao.com>
2023-10-02 08:46:31 +0900

    filter 기능  - 생각 외로 많이 사용하지 않는다.

---
+++ src/test/java/hello/core/scan/filter/MyIncludeComponent.java
@@ -0,0 +1,12 @@
+package hello.core.scan.filter;
+
+
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE) // 클래스 레벨에 붙는 것!
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MyIncludeComponent {
+}
