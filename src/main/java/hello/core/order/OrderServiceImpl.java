64923333d0d8d6aadc2e9ea5a2eb8f4b92b130f0
hongsh429 <shh0429@kakao.com>
2023-10-02 09:51:13 +0900

    의존관계 주입 방법  - 생성자 주입  - 수정자 주입(setter 주입)  - 필드 주입       > 순수 자바코드로 테스트 할 때, getter/setter 가 없으면 테스트 불가능하다. 인텔리제이에서도 권장하지 않는 방법이다.       > 거의 테스트 코드에서만 사용하고, 그 외에선 잘 사용하지 않는다.  - 일반 메서드 주입

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -14,13 +14,13 @@ public class OrderServiceImpl implements OrderService {
     // field DI : 필드를 통해 주입한다.
     // 특징 - Field injection is not recommended <- intellij 에서 추천하지 않는다는 문구.
     // 테스트 할 때,
-    @Autowired private MemberRepository memberRepository;
-    @Autowired private DiscountPolicy discountPolicy;
+    /*@Autowired*/ private final MemberRepository memberRepository;
+    /*@Autowired*/ private final DiscountPolicy discountPolicy;
 
-//    // ============
-//    // setter DI : setter 메서드를 통해 주입된다.
-//    // 특징 - 변경 가능성이 존재하는 의존성에 사용 된다!
-//    //     - 생성자 주입보다 나중에 일어난다. (당연)
+    // ============
+    // setter DI : setter 메서드를 통해 주입된다.
+    // 특징 - 변경 가능성이 존재하는 의존성에 사용 된다!
+    //     - 생성자 주입보다 나중에 일어난다. (당연)
 //    @Autowired
 //    public void setMemberRepository(MemberRepository memberRepository) {
 //        System.out.println("2. 세터 주입");
@@ -32,16 +32,16 @@ public class OrderServiceImpl implements OrderService {
 //        System.out.println("3. 세터 주입");
 //        this.discountPolicy = discountPolicy;
 //    }
-//
-//    // =============
-//    // constructor DI : 생성자 주입
-//    // 특징 - 한번 주입이 일어나면, 변경 불가능 (불가능)
-//    //    @Autowired  <- 생성자가 1개만 존재할 경우, @Autowired 생략 가능
-//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-//        System.out.println("1. OrderServiceImpl.OrderServiceImpl");
-//        this.memberRepository = memberRepository;
-//        this.discountPolicy = discountPolicy;
-//    }
+
+    // =============
+    // constructor DI : 생성자 주입
+    // 특징 - 한번 주입이 일어나면, 변경 불가능 (불가능)
+    //    @Autowired  <- 생성자가 1개만 존재할 경우, @Autowired 생략 가능
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        System.out.println("1. OrderServiceImpl.OrderServiceImpl");
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
--- src/test/java/hello/core/order/OrderServiceTest.java
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -35,12 +35,12 @@ public class OrderServiceTest {
         Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
     }
 
-    @Test
-    void fieldInjectionTest() {
-        OrderService orderService = new OrderServiceImpl();
-        orderService.createOrder(1L, "itemA", 10000);
-        // 순수 자바 코드로 test를 해보고 싶을 때, 이렇게 필드 주입을 하면, 불가능하다!
-        // 왜? 스프링은 @Autowired를 통해서 주입해주었지만, 실제로 내가 인스턴스를 호출해서 뭔가 테스트를 할 때, 불가능 하다.
-        // 이것을 가능하게 하려면, getter/setter 를 열어주어서 세팅해주어야 한다.
-    }
+//    @Test
+//    void fieldInjectionTest() {
+//        OrderService orderService = new OrderServiceImpl();
+//        orderService.createOrder(1L, "itemA", 10000);
+//        // 순수 자바 코드로 test를 해보고 싶을 때, 이렇게 필드 주입을 하면, 불가능하다!
+//        // 왜? 스프링은 @Autowired를 통해서 주입해주었지만, 실제로 내가 인스턴스를 호출해서 뭔가 테스트를 할 때, 불가능 하다.
+//        // 이것을 가능하게 하려면, getter/setter 를 열어주어서 세팅해주어야 한다.
+//    }
 }
