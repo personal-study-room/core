dcb505f28822bb0672c14a1c1147308024405017
hongsh429 <shh0429@kakao.com>
2023-10-02 16:34:23 +0900

    annotation의 활용  - 필요한 annotation을 정의하여 사용한다.  - 내가 Qualifier를 사용한다면? -> 그에 맞는 어노테이션을 만들어 사용한다.    왜? 매번 @Qualifier("~~")라고하면, compile에서 에러를 잡을 수 없다. 퀄리파이어는 sub로 사용되어지는데,    문제가 발생하는 시점이 해당 어노테이션이 붙고 잘못 입력한 곳에서 발생하므로, 이를 방지하고자 하나에서 관리를    하기 위함. 어찌보면 SRP를 준수하기 위해서 사용하는 방법  ---  annotation은 원래 조합으로 사용하는 것. 상속의 개념이 존재하지 않는다.  단, 무분별하게 하지 않는 것이 중요.

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,10 +1,9 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,7 +13,7 @@ public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    public OrderServiceImpl(MemberRepository memberRepository, @Qualifier("mainDiscountPolicy")DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
