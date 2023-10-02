750b2b639d6c38fa6e4d894b3d9aca52bc535b17
hongsh429 <shh0429@kakao.com>
2023-10-02 16:25:06 +0900

    @Autowired에서 조회 빈이 2개 이상일 경우!  - @Autowired 필드 명 매칭      1. 타입비교      2. 변수(필드, 파라미터) 이름과 빈 이름 비교  - @Qualifier @Qualifier끼리 매칭 빈 이름 매칭  - @Primary 사용  ---  활용 방안  메인으로 사용하는 곳에 @Primary사용. 그리고 서브로 사용하는 몇몇 위치에는 @Qualifier를 사용  **Spring은 무조건 구체적으로 지정해주는 것이 우선 순위가 높다. 그래서 @Primary보단 @Qualifier가 더 우선순위가 높다.**

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,42 +3,18 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+//@RequiredArgsConstructor // final field를 가지고 만들어준다.!
 public class OrderServiceImpl implements OrderService {
 
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
 
-    // ============
-    // field DI : 필드를 통해 주입한다.
-    // 특징 - Field injection is not recommended <- intellij 에서 추천하지 않는다는 문구.
-    // 테스트 할 때,
-    /*@Autowired*/ private final MemberRepository memberRepository;
-    /*@Autowired*/ private final DiscountPolicy discountPolicy;
-
-    // ============
-    // setter DI : setter 메서드를 통해 주입된다.
-    // 특징 - 변경 가능성이 존재하는 의존성에 사용 된다!
-    //     - 생성자 주입보다 나중에 일어난다. (당연)
-//    @Autowired
-//    public void setMemberRepository(MemberRepository memberRepository) {
-//        System.out.println("2. 세터 주입");
-//        this.memberRepository = memberRepository;
-//    }
-//
-//    @Autowired
-//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
-//        System.out.println("3. 세터 주입");
-//        this.discountPolicy = discountPolicy;
-//    }
-
-    // =============
-    // constructor DI : 생성자 주입
-    // 특징 - 한번 주입이 일어나면, 변경 불가능 (불가능)
-    //    @Autowired  <- 생성자가 1개만 존재할 경우, @Autowired 생략 가능
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        System.out.println("1. OrderServiceImpl.OrderServiceImpl");
+    public OrderServiceImpl(MemberRepository memberRepository, @Qualifier("mainDiscountPolicy")DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
@@ -56,6 +32,7 @@ public class OrderServiceImpl implements OrderService {
     public MemberRepository getMemberRepository() {
         return memberRepository;
     }
+
     // 테스트 용도
     public DiscountPolicy getDiscountPolicy() {
         return discountPolicy;
